587c6c3738a9aefb79b67220ed5b7d97a2b41299
lianyu <1319764245@qq.com>
2024-06-07 10:23:24 +0800

    封装通用弹框组件

---
+++ src/components/CustomDialog/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-dialog
+    :title="title"
+    :visible.sync="dialogVisible"
+    :width="width"
+    :fullscreen="fullscreen"
+    @close="handleClose"
+  >
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="handleClose">取 消</el-button>
+      <el-button type="primary" @click="onConfirm">确 定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: String,
+      default: '50%'
+    },
+    fullscreen: {
+      type: Boolean,
+      default: false
+    },
+    onConfirm: {
+      type: Function,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleClose() {
+      this.dialogVisible = false
+    },
+    issueEvent(value, mouseEvent) {
+      if (mouseEvent) {
+        return mouseEvent(value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
