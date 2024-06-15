cf238a1ca48c2b7f6040667bb4fe06fd956d9bb8
lianyu <1319764245@qq.com>
2024-06-15 11:14:05 +0800

    封装树形组件

---
+++ src/components/CustomTree/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-tree
+    :data="data"
+    :show-checkbox="showCheckBox"
+    node-key="id"
+    default-expand-all
+    :expand-on-click-node="false"
+    :current-node-key="currNodeKey"
+    :highlight-current="ishighlightCurrent"
+    :style="`width:${width}`"
+  >
+    <el-row slot-scope="{ data }" style="width: 100%; height: 40px" class="custom-tree-node">
+      <el-col>{{ data.name }}</el-col>
+      <slot name="right" :row="data" />
+    </el-row>
+  </el-tree>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      default: ''
+    },
+    showCheckBox: {
+      type: Boolean,
+      default: false
+    },
+    data: {
+      type: Array,
+      default: () => []
+    },
+    currNodeKey: {
+      type: Number,
+      default: null
+    },
+    ishighlightCurrent: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.custom-tree-node {
+  // width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
